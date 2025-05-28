15826685888ca6f23da7ec691f8f021c84952448
nigarvalieva <vvaliyevaa@gmail.com>
2025-05-28 20:41:05 +0400

    Changes in main.py

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-from ultralytics import YOLO
-from PIL import Image
 import cv2
+from ultralytics import YOLO
+import signal
 
 model = YOLO("best.pt")
-result = model.predict(source="0", show=True)
+running = True
+
+def signal_handler(sig, frame):
+    global running
+    print("\nStopped by user (keys 'CTRL+C' pressed).")
+    running = False
+
+signal.signal(signal.SIGINT, signal_handler)
+
+cap = cv2.VideoCapture(0)
+
+while running:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    results = model.predict(source=frame, conf=0.25, verbose=False)
+
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("YOLO - Press 'CTRL+C' to exit", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Stopped by user (keys 'CTRL+C' pressed).")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
